501e7642122cbe24740ce614c325581aebe6b4b0
Zyrian Gantuangco <g.zyrian@gmail.com>
2019-09-13 17:35:22 -0400

    Moved home component to components folder

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './compontents/home/home.component';
 
 
 const routes: Routes = [
